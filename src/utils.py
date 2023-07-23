38f6ebe08edd3408523ba7ee69588a329a3e678b
Adrianova Rina <107701817+rinaadrianova@users.noreply.github.com>
2023-07-23 19:10:45 +0300

    second version

--- src/utils.py
+++ src/utils.py
@@ -4,15 +4,6 @@ from datetime import datetime
 with open('operations.json', 'r', encoding='utf-8') as f:
     data = json.load(f)
 
-def executed_transaction():
-    count = 0
-    last_five = []
-    while count < 5:
-        if data[count].get('state') == "EXECUTED":
-            last_five.append(data[count])
-            count += 1
-    return last_five
-
 
 def transfer_date(date):
     '''
@@ -25,18 +16,22 @@ def transfer_date(date):
     formatted_date = dateobj.strftime('%d.%m.%Y')
     return formatted_date
 
-def transaction_cards(card_number):
+def transaction_cards(card_number=None):
     '''
     Принимает имя и номер карты или счета и возвращает
     в отформатированном виде
     :param card_number:
     '''
-    if 'Счет' not in card_number:
-        card_num = card_number[-16:]
-        card_name = card_number[:-16].rstrip()
-        return f'{card_name} {card_num[:4]} {card_num[4:6]}** **** {card_number[-4:]}'
+    if card_number is None:
+        return 'Не указано'
     else:
-        card_num = card_number[-20:]
-        card_name = card_number[:-20].rstrip()
-        return f'{card_name} **{card_num[-4:]}'
+        if 'Счет' not in card_number:
+            card_num = card_number[-16:]
+            card_name = card_number[:-16].rstrip()
+            return f'{card_name} {card_num[:4]} {card_num[4:6]}** **** {card_number[-4:]}'
+        else:
+            card_num = card_number[-20:]
+            card_name = card_number[:-20].rstrip()
+            return f'{card_name} **{card_num[-4:]}'
+
 
